a64128e86abd2cef22b44d00611a96c5a4c978c7
Ash <ashwani@hocalwire.com>
2016-05-25 16:49:06 +0530

    modified express view cache file

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var curl = require('request'),
   url = require('url'),
   async = require('async'),
   crypto = require('crypto'),
-  redis = require('redis');
+  redis = require('redis'),
+  MobileAgent = require('mobile-agent');
 
 /**
  * @module express-view-cache
@@ -30,7 +31,7 @@ function EVC(options) {
     if (o.protocol === 'redis:') {
       config.host = o.hostname || 'localhost';
       config.port = o.port || 6379;
-      config.pass = o.auth ? o.auth.split(':')[1] : null;
+      config.pass = o.auth ? o.auth[1] : null;
       config.appPort = process.env.PORT || 3000;
     } else {
       throw new Error('ExpressViewCache - unable to parse ' + o + ' as redis connection string!');
@@ -53,24 +54,74 @@ function EVC(options) {
   /**
    * @method EVC#cachingMiddleware
    * @param {Number} [ttlInMilliSeconds=30000]
+   * @param {Array} queryParams cache based on query parameter
+   * @param {Boolean} [isResponsive=true]
    * @return {function} function(req, res, next){...}
    */
 
-  this.cachingMiddleware = function (ttlInMilliSeconds) {
+  this.cachingMiddleware = function (ttlInMilliSeconds,queryParams,followRedirection, isResponsive,partnerPassed) {
     ttlInMilliSeconds = parseInt(ttlInMilliSeconds, 10) || 30000;
+    isResponsive = typeof isResponsive === "undefined" ? false : isResponsive;
 
     return function (req, res, next) {
+      var partner = req.headers['partner'];
+      if(!partner){ //to be removed code as it will come ngnix
+          var domain = req.vhost.hostname;
+          if(domain.indexOf("partner1.com")>-1){
+              partner = "partner1";
+          } else if(domain.indexOf("partner2.com")>-1){
+              partner = "partner2";
+          } else {
+              partner = "hocal";
+          }
+      }
+      if(partnerPassed!=partner){
+        next();
+        return;
+      }
+
       if (req.method === 'GET' && req.headers.express_view_cache !== cacheKey) {
-        var key = req.originalUrl,
+        var urlToUse = req.originalUrl;
+
+        var responsiveSuffix = '';
+          // get device type (mobile/desktop)
+        if (!isResponsive) {
+            responsiveSuffix = isMobile(req) ? ':mobile' : ':desktop';
+        }
+
+        var query = req.query;
+        if(queryParams) {
+          if(queryParams.length===0){
+            urlToUse=req.originalUrl.split("?")[0];
+          } else {
+            var queryParamsFinal="";
+            var count = 0;
+            for(var i=0;i<queryParams.length;i++){
+              if(query[queryParams[i]]){
+                queryParamsFinal+=(count==0?"?":"&")+queryParams[i]+"="+query[queryParams[i]];
+                count++;
+              }
+            }
+            if(req.originalUrl.indexOf("?")){
+              urlToUse=req.originalUrl.split("?")[0]+queryParamsFinal;
+            } else {
+              urlToUse=req.originalUrl+queryParamsFinal;
+            }
+          }
+        }
+
+        var key = urlToUse,
+            responsiveKey = urlToUse + responsiveSuffix,
           data = {};
+          console.log("key used in caching module================="+ key);
         async.waterfall([
           function (cb) {
             async.parallel({
               'dataFound': function (clb) {
-                redisClient.hgetall(key, clb);
+                redisClient.hgetall(responsiveKey, clb);
               },
               'age': function (clb) {
-                redisClient.ttl(key, clb);
+                redisClient.ttl(responsiveKey, clb);
               }
             }, function (error, obj) {
               if (error) {
@@ -87,14 +138,20 @@ function EVC(options) {
               data['Content-Type'] = dataFound.contentType;
               data.statusCode = dataFound.statusCode;
               data.content = dataFound.content;
+              data.cookie = res._headers['set-cookie'];
               cb(null, true);
             } else {
               var headers = req.headers;
+              var flagFollowRedirection = false;
+              if(followRedirection){
+                flagFollowRedirection = followRedirection;
+              }
               headers.express_view_cache = cacheKey;
               curl({
                 'method': 'GET',
                 'headers': headers,
-                'url': 'http://localhost:' + config.appPort + key
+                'url': 'http://localhost:' + config.appPort + req.originalUrl,
+                'followRedirect': flagFollowRedirection
               }, function (error, response, body) {
                 if (error) {
                   cb(error);
@@ -104,26 +161,37 @@ function EVC(options) {
                   data['Content-Type'] = response.headers['content-type'];
                   data.statusCode = response.statusCode;
                   data.content = body;
+                  if(data.statusCode===301) {
+                    data.redirectUrl = response.headers.location;
+                  }
+
+                  data['cookie'] = response.headers['set-cookie'];
+
                   cb(error, false);
                 }
               });
             }
           },
+
           function (hit, cb) {
             if (hit) {
               cb(null);
             } else {
               async.series([
                 function (clb) {
-                  redisClient.hmset(key, {
-                    'savedAt': new Date(),
-                    'contentType': data['Content-Type'],
-                    'statusCode': data.statusCode,
-                    'content': data.content
-                  }, clb);
+                  if(data.statusCode == 200){
+                    redisClient.hmset(responsiveKey, {
+                      'savedAt': new Date(),
+                      'contentType': data['Content-Type'],
+                      'statusCode': data.statusCode,
+                      'content': data.content
+                    }, clb);
+                  }else{
+                    clb();
+                  }
                 },
                 function (clb) {
-                  redisClient.expire(key, Math.floor(ttlInMilliSeconds / 1000), clb);
+                  redisClient.expire(responsiveKey, Math.floor(ttlInMilliSeconds / 1000), clb);
                 }
               ], cb);
             }
@@ -132,11 +200,20 @@ function EVC(options) {
           if (error) {
             next(error);
           } else {
-            res.set('Expires', data.Expires);
-            res.set('Last-Modified', data['Last-Modified']);
-            res.set('Content-Type', data['Content-Type']);
-            res.status(data.statusCode);
-            res.send(data.content);
+            if(data.statusCode===301) {
+                return res.redirect(301,data.redirectUrl);
+              } else {
+                if(data.cookie){
+                  res.setHeader('Set-Cookie', data.cookie);  
+                }
+                
+                res.set('Expires', data.Expires);
+                res.set('Last-Modified', data['Last-Modified']);
+                res.set('Content-Type', data['Content-Type']);
+                res.status(data.statusCode);
+
+                res.send(data.content);
+            }
           }
         });
       } else {
@@ -145,6 +222,16 @@ function EVC(options) {
     };
   };
   return this;
+}
+
+/**
+ * Returns boolean value for whether the client is using mobile/desktop browser
+ * @param  {Object}  req
+ * @return {Boolean} isMobile[true/false]
+ */
+function isMobile(req) {
+    var agent = MobileAgent(req.headers["user-agent"]);
+    return agent.Mobile;
 }
 
 module.exports = exports = function (config) {
